5dd8e5b071666fef7d4259069857036f19446ce9
Alfonso <alfof87@gmail.com>
2021-03-18 12:48:46 +0100

    front-end - 5

--- resources/views/graphic.blade.php
+++ resources/views/graphic.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.main-layout')
 @section('content')
   @auth
-    <div class="chart">
-      <canvas id="myChart"></canvas>
+    <div class="chart-container">
+      <div class="chart">
+        <canvas id="myChart"></canvas>
+      </div>
+      <p>Business performance from 2015 to 2020.</p>
     </div>
   @else
     <div class="log">
